8339d35c59068665a6b7c9f6ab422c6253f0d091
koushik-m <koushik2498@gmail.com>
2019-05-21 14:38:00 +0530

    fixed navbar

--- asset_tracker/src/Components/Navbar/index.js
+++ asset_tracker/src/Components/Navbar/index.js
@@ -7,12 +7,12 @@ import { Link } from 'react-router-dom'
 export default class Navbar extends React.Component {
   render () {
     return (
-      <div class="navbar">
-        <Link className="active" to="/user"><i class="fa fa-fw fa-home"></i> Assets-Tracker</Link>
+      <div className="navbar">
+        <Link className="active" to="/user"><i className="fa fa-fw fa-home"></i> Assets-Tracker</Link>
         <Link to="/user/profile"><i class="fa fa-fw fa-user"></i> </Link>
         <Link to="/user/notifications"><i class="fa fa-fw fa-bell"></i> </Link>
         <Link to="/user/devices"><i class="fa fa-fw fa-mobile"></i> </Link>
-        <Link to="/"><Button btnprop='btn btn-danger'>Log out</Button></Link>
+        <Link className="link" to="/"><Button btnprop='btn btn-danger'>Log out</Button></Link>
       </div>
     )
   }
